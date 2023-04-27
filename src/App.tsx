be2bfc2b8885798c3346d38e2782d3e8fdc74d61
calm1205 <naginani072@gmail.com>
2023-04-27 15:08:47 +0900

    pdf download

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
-import { PDFViewer } from "@react-pdf/renderer";
+import { PDFViewer, PDFDownloadLink } from "@react-pdf/renderer";
 import { MyDocument } from "./MyDocument";
 
 export const App: FC = () => {
   return (
-    <PDFViewer>
-      <MyDocument />
-    </PDFViewer>
+    <>
+      <h1>react-pdf/render</h1>
+
+      <PDFViewer>
+        <MyDocument />
+      </PDFViewer>
+
+      <br />
+
+      <PDFDownloadLink document={<MyDocument />} fileName="適性検査結果.pdf">
+        PDFのダウンロード
+      </PDFDownloadLink>
+    </>
   );
 };
