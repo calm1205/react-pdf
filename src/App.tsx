4b5d975943e3365f90e1f8c927f6ad0d41f9427f
calm1205 <naginani072@gmail.com>
2023-04-27 15:04:29 +0900

    install react-pdf/reander

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import { FC } from "react";
+import { PDFViewer } from "@react-pdf/renderer";
+import { MyDocument } from "./MyDocument";
 
-export const App: React.FC = () => {
-  const [numPages, setNumPages] = useState<number | null>(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const onDocumentLoadSuccess: (args: { numPages: number }) => void = ({
-    numPages,
-  }) => {
-    setNumPages(numPages);
-  };
-
+export const App: FC = () => {
   return (
-    <div>
-      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
+    <PDFViewer>
+      <MyDocument />
+    </PDFViewer>
   );
 };
